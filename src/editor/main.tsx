d8951d5a413c952eee34c8eaef8a1ddcf7db6fb7
falcoer <38299246+falcoer@users.noreply.github.com>
2026-09-16 22:40:36 +0200

    feat(proto): enable PROTO-01 launcher

--- src/editor/main.tsx
+++ src/editor/main.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App.js";
+import { enablePrototypeLauncher } from "./prototypeLauncher.js";
 import "./style.css";
+
 createRoot(document.getElementById("root")!).render(<App />);
+enablePrototypeLauncher();
